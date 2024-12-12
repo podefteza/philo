2795d048500c5652f86a3c1b53711c9118159d1c
podefteza <carlosjorgesp43@gmail.com>
2024-12-12 10:18:12 +0000

    1

---
+++ main.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carlos-j <carlos-j@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 13:11:19 by carlos-j          #+#    #+#             */
+/*   Updated: 2024/12/11 14:00:37 by carlos-j         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+./philo number_of_philosophers time_to_die time_to_eat time_to_sleep
+								+ [number_of_times_each_philosopher_must_eat]
+
+
+MAX 200 philos
+
+Makefile with -pthread
+
+Allowed functions:
+memset
+printf
+malloc
+free
+write
+usleep
+gettimeofday
+pthread_create
+pthread_detach
+pthread_join
+pthread_mutex_init
+pthread_mutex_destroy
+pthread_mutex_lock
+pthread_mutex_unlock
+
+*/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+int	main()
+{
+	struct timeval current_time;
+	gettimeofday(&current_time, NULL);
+	printf("%ld\n", current_time.tv_sec);
+
+
+	/*
+	args = 4 || 5 (+1)
+	first checks: only ints
+
+	nr of philos > 0 <= 200
+	time to die > 0
+	time to eat > 0
+	time to sleep > 0
+	optional arg times to eat >= 0
+	*/
+
+}
+
+
+/*
+◦ timestamp_in_ms X has taken a fork
+◦ timestamp_in_ms X is eating
+◦ timestamp_in_ms X is sleeping
+◦ timestamp_in_ms X is thinking
+◦ timestamp_in_ms X died
+*/
