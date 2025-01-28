9697870dfa1651d893a62b83e124924998e03977
carlos-j <carlos-j@student.42porto.com>
2025-01-28 14:54:08 +0000

    202501281454

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: carlos-j <carlos-j@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 11:24:35 by carlos-j          #+#    #+#             */
-/*   Updated: 2025/01/28 13:11:23 by carlos-j         ###   ########.fr       */
+/*   Updated: 2025/01/28 14:51:57 by carlos-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ typedef struct s_philos
 	int				id;
 	int				meals;
 	long long		last_meal;
-	pthread_t		thread;
+	pthread_t		thread; // one thread per philo -> not initialized in init()
 	pthread_mutex_t	*left_fork;
 	pthread_mutex_t	*right_fork;
 	pthread_mutex_t	meals_lock;
@@ -50,12 +50,12 @@ typedef struct s_setup
 	struct timeval	start_time;
 	long long		elapsed_time;
 	//pthread_mutex_t	time_lock;
-	pthread_mutex_t	*forks;
+	pthread_mutex_t	*forks; // does it make sense to be inside this struct or move to t_philos
 	pthread_mutex_t	write_lock;
 	pthread_mutex_t	stop_lock;
 	int				stop;
-	int				all_eaten;
-	int				is_dead;
+	//int				all_eaten; // not being used...?
+	// int				is_dead; // not being used...?
 }					t_setup;
 
 // utils.c
@@ -79,7 +79,7 @@ int					philo_sleeping(t_philos *philo);
 int					philo_thinking(t_philos *philo);
 
 // philo_checker.c
-int					check_all_philosophers_eaten(t_setup *setup);
+//int					check_all_philosophers_eaten(t_setup *setup);
 int					check_philosopher_status(t_setup *setup, int i,
 						long long *last_meal, int *total_meals);
 int					check_all_philosophers(t_setup *setup, int *total_meals);
